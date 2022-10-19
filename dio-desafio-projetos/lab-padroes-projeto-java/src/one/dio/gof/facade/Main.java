ba7fccb92c355212c392801299cd43a85d506be2
arduinobymyself <arduinobymyself@gmail.com>
2022-10-19 13:42:56 -0300

    subindo arquivos FACADE

---
+++ dio-desafio-projetos/lab-padroes-projeto-java/src/one/dio/gof/facade/Main.java
@@ -0,0 +1,9 @@
+package one.dio.gof.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Marcelo Moraes", "18050-000");
+
+    }
+}
